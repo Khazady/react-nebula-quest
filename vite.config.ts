c3e42508bf7c1f085db459e975d3c8f63b21bf8f
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-09-15 13:40:28 +0200

    build(vite): add svgr and optimize SVG processing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: '/react-nebula-quest/',
-  plugins: [react()],
+  base: "/react-nebula-quest/",
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        svgoConfig: {
+          floatPrecision: 2,
+          plugins: [
+            {
+              name: "cleanupIds",
+            },
+            {name: "convertPathData"},
+          ],
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
