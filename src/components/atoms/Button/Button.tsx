eef8075c4ac46e92c3fcda77f58eb6e1b14aac26
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-09-16 23:52:30 +0200

    feat(Button): add primary variant to Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -2,9 +2,9 @@ import {cn} from "@/lib/utils/cn.ts";
 import type {ButtonProps} from "./Button.types";
 import "./Button.sass";
 
-export const Button: React.FC<ButtonProps> = ({type = 'button', className, onClick, children}) => {
+export const Button: React.FC<ButtonProps> = ({type = 'button', className, onClick, children, variant}) => {
   return (
-    <button type={type} className={cn('button', className)} onClick={onClick}>
+    <button type={type} className={cn('button', variant && `button--${variant}`, className)} onClick={onClick}>
       {children}
     </button>
   );
--- src/components/atoms/Button/Button.types.ts
+++ src/components/atoms/Button/Button.types.ts
@@ -1,6 +1,8 @@
+export type ButtonVariant = 'primary';
+
 export type ButtonProps = React.PropsWithChildren<{
   type?: React.ComponentProps<"button">["type"];
   className?: React.ComponentProps<"button">["className"];
   onClick?: React.ComponentProps<"button">["onClick"];
+  variant?: ButtonVariant;
 }>;
-
