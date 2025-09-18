10771a823cc40d9ea25d3666edcf8680dffcd767
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-09-18 16:01:41 +0200

    feat(Offers): add Offers componen

--- src/components/organisms/Offers/Offers.tsx
+++ src/components/organisms/Offers/Offers.tsx
@@ -1,3 +1,27 @@
+import card1 from "@/assets/images/card-1.png";
+import card2 from "@/assets/images/card-2.png";
+import card3 from "@/assets/images/card-3.png";
+import card4 from "@/assets/images/card-4.png";
+import {OfferCard} from "@/components/molecules";
+import {dictionary} from "@/lib/dictionary.ts";
+import "./Offers.sass";
+
 export const Offers = () => {
-  return <section>Hero</section>;
+  const {offers} = dictionary;
+  const images = [card1, card2, card3, card4];
+
+  return <section className="offers">
+    <h2 className="offers__title">{offers.title}</h2>
+    {offers.items.map((offer, idx) => {
+      const isFeatured = idx === 0 || idx === offers.items.length - 1;
+      return (
+        <OfferCard
+          key={idx}
+          {...offer}
+          imageUrl={images[idx % images.length]}
+          featured={isFeatured}
+        />
+      );
+    })}
+  </section>;
 };
