85d64d6ba0914a755fa096e5d79f45b6be8259d2
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-09-18 17:51:36 +0200

    feat(Header): add hover effect for cart icon and include colored SVG

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,38 +1,50 @@
 import LogoIcon from "@/assets/brand/Logo.svg?react";
 import CartIcon from "@/assets/icons/Cart.svg?react";
-import {Link} from "@/components/atoms";
-import {BurgerButton, Drawer, NavItems} from "@/components/molecules";
+import CartColoredIcon from "@/assets/icons/CartСolored.svg?react";
+import { Link } from "@/components/atoms";
+import { BurgerButton, Drawer, NavItems } from "@/components/molecules";
 import "./Header.sass";
-import {dictionary} from "@/lib/dictionary.ts";
-import {useState} from "react";
+import { dictionary } from "@/lib/dictionary.ts";
+import { useState } from "react";
 
 export const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const navLinks = [
     ...dictionary.header.nav.map((label) => (
-      <Link key={label} href="#">{label}</Link>
+      <Link key={label} href="#">
+        {label}
+      </Link>
     )),
     <Link key="cart" href="#" className="header__cart">
-      {/*todo: hover gradient*/}
-      <CartIcon className="header__cart-icon"/>
-    </Link>
+      <CartIcon className="header__cart-icon header__cart-icon--default" />
+      <CartColoredIcon className="header__cart-icon header__cart-icon--hover" />
+    </Link>,
   ];
 
   return (
     <header className="header">
-      <Link href="/" className='header__logo'>
-        <LogoIcon className="header__logo-icon"/>
+      <Link href="/" className="header__logo">
+        <LogoIcon className="header__logo-icon" />
       </Link>
-      <BurgerButton open={isDrawerOpen} onToggle={() => setIsDrawerOpen((o) => !o)}/>
+      <BurgerButton
+        open={isDrawerOpen}
+        onToggle={() => setIsDrawerOpen((o) => !o)}
+      />
 
       <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
-        <NavItems orientation="vertical" className="header__nav header__nav--mobile">
+        <NavItems
+          orientation="vertical"
+          className="header__nav header__nav--mobile"
+        >
           {navLinks}
         </NavItems>
       </Drawer>
 
-      <NavItems orientation="horizontal" className="header__nav header__nav--desktop">
+      <NavItems
+        orientation="horizontal"
+        className="header__nav header__nav--desktop"
+      >
         {navLinks}
       </NavItems>
     </header>
